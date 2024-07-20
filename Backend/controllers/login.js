ec75808fa6634e07f4305a1b65da0b241c137772
RitickLath <ritick943lath@gmail.com>
2024-07-21 03:08:16 +0530

    implemented login

--- Backend/controllers/login.js
+++ Backend/controllers/login.js
@@ -1,1 +1,44 @@
-exports.login = async (req, res) => {};
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Checking if there is a user with the provided email
+    const userDetail = await User.findOne({ email });
+    if (!userDetail) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    // Verify password
+    const passwordVerify = await bcrypt.compare(password, userDetail.password);
+    if (!passwordVerify) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const userId = userDetail._id;
+    const payload = { userId };
+    req.userId = userId;
+    const token = jwt.sign(payload, process.env.SECRET, { expiresIn: "1h" }); // Token expiration time can be adjusted
+
+    return res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      token,
+    });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      success: false,
+      message: "An error occurred while logging in the user",
+    });
+  }
+};
