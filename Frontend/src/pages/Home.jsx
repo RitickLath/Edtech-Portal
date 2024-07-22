4ea6e9a97997bfc3aca8fd88e1daea296efdd3c3
RitickLath <ritick943lath@gmail.com>
2024-07-22 05:56:30 +0530

    hero section

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-full h-full min-h-screen py-12 bg-[#000814] text-white md:flex md:justify-between px-10 lg:px-14">
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
