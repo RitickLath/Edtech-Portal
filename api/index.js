5b730123155dc42c6dd74f889d6fb68e77dff5dc
RitickLath <ritick943lath@gmail.com>
2024-08-14 17:15:47 +0530

    g

--- api/index.js
+++ api/index.js
@@ -1,7 +1,27 @@
+const express = require("express");
+const app = express();
+const BasicRouter = require("./routes/BasicRoute");
+
+const { connect } = require("./config/database");
+const cors = require("cors");
+
+require("dotenv").config();
+connect();
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/api/v1", BasicRouter);
+
+// Default to port 3000 if PORT is not set
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
 // const express = require("express");
 // const app = express();
 // const BasicRouter = require("./routes/BasicRoute");
-
 // const { connect } = require("./config/database");
 // const cors = require("cors");
 
@@ -14,22 +34,3 @@
 // app.use("/api/v1", BasicRouter);
 
 // module.exports = app;
-// app.listen(3000, () => {
-//   console.log("Listening");
-// });
-
-const express = require("express");
-const app = express();
-const BasicRouter = require("./routes/BasicRoute");
-const { connect } = require("./config/database");
-const cors = require("cors");
-
-require("dotenv").config();
-connect();
-app.use(cors());
-app.use(express.json());
-
-// routes
-app.use("/api/v1", BasicRouter);
-
-module.exports = app;
