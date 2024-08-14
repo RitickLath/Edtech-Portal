00d22ad2347c001b1bfc6e14128a2d7142f0618a
RitickLath <ritick943lath@gmail.com>
2024-08-14 17:22:38 +0530

    g

--- api/index.js
+++ api/index.js
@@ -10,27 +10,9 @@ connect();
 app.use(cors());
 app.use(express.json());
 
-// routes
 app.use("/api/v1", BasicRouter);
 
-// Default to port 3000 if PORT is not set
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
-// const express = require("express");
-// const app = express();
-// const BasicRouter = require("./routes/BasicRoute");
-// const { connect } = require("./config/database");
-// const cors = require("cors");
-
-// require("dotenv").config();
-// connect();
-// app.use(cors());
-// app.use(express.json());
-
-// // routes
-// app.use("/api/v1", BasicRouter);
-
-// module.exports = app;
